87da57dd8bd31c64607ad2cb99e6e11ab1a766e6
Waqas Ahmad <waqasahmedwikki15@gmail.com>
2024-03-25 21:38:18 +0500

    object return

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+var students = {
+  name1: "waqas",
+  rollNumber: 415,
+  course: "html",
+  institute: "smit",
+};
 
+function addStudent(name1, rollNumber, course, institute) {
+  return {
+    name1: name1,
+    rollNumber: rollNumber,
+    course: course,
+    institute: institute,
+  };
+}
+var showStudent = [
+  addStudent("waqas", 42, "js", "smit"),
+  addStudent("munir", 45, "css", "smit"),
+];
+console.log(showStudent);
